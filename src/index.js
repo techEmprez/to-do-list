295c80da82a3a8c5390b3e13e61655bb622fa1a0
techEmprez <suigeneriz001@gmail.com>
2022-08-12 13:10:54 +0100

    add template class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './style.css'
+import './style.css';
+
+class Template {
+  constructor(description, completed, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
+}
